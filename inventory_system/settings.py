badc5c4c34b34bbbaf7e0a23f2af6d681dcdee9c
amna <amnaaimen123@gmail.com>
2026-07-10 02:38:55 +0500

    Add whitenoise for static files

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -54,13 +54,27 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+
+
+
+
+
+
+
+
     'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware', # Ye line yahan add karein
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+
+
+
 ]
 
 ROOT_URLCONF = 'inventory_system.urls'
